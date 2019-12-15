05808dc58dd6b08cc64e4e784bbd80a6ecfe634b
waiyanpaing <waiyanpaing2027@gmail.com>
2019-12-15 20:02:16 +0630

    Update

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,3 +1,10 @@
 print("Hello World")
 
-1 2 3 4 5
+#1 2 3 4 5
+
+# this is the first comment
+
+spam = 1  # and this is the second comment
+			# ... and now a third!
+text = "# This is not a comment because it's inside quotes."
+print(text)
